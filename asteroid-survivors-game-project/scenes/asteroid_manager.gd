5fa41c657bea6d5562f981f8bba2d4ed22bc7c2e
GreebGames <56564634+EvanHecht@users.noreply.github.com>
2025-11-19 22:24:36 -0600

    Added epic asteroid modifier logic

--- asteroid-survivors-game-project/scenes/asteroid_manager.gd
+++ asteroid-survivors-game-project/scenes/asteroid_manager.gd
@@ -5,15 +5,30 @@ const asteroid_scene: PackedScene = preload("res://scenes/asteroid.tscn")
 @onready var spawn_timer: Timer = $spawn_timer
 
 var spawn_offset: float = 460
+var wave_budget: int = 100
+var min_budget_for_group: int = 25
+var current_wave = 1
+var max_modifiers = 1
+var modifier_chance_increase_per_wave = 0.1
+
 @export var spawn_angle_spread: float = 1.0
 
 func _ready() -> void:
-	spawn_timer.timeout.connect(spawn_asteroid)
+	spawn_timer.timeout.connect(start_wave)
+
+func start_wave():
+	while (wave_budget >= min_budget_for_group):
+		spawn_asteroid()
 
 func spawn_asteroid():
 	var center: Vector2 = get_viewport_rect().size / 2
 	var spawn_point: Vector2 = center + (Vector2.from_angle(randf() * TAU).normalized()) * spawn_offset
 	var asteroid: Asteroid = asteroid_scene.instantiate()
+	var modifier_chance: float = randf();
+	var num_modifiers = floor(pow((modifier_chance + 1 - modifier_chance_increase_per_wave * current_wave), -2))
+	for i in range(0, num_modifiers):
+		asteroid.apply_modifier()
 	asteroid.global_position = spawn_point
-	asteroid.velocity = Vector2.from_angle((center - spawn_point).angle() + randf_range(-spawn_angle_spread, spawn_angle_spread)) * 100
+	asteroid.velocity = Vector2.from_angle((center - spawn_point).angle() + randf_range(-spawn_angle_spread, spawn_angle_spread)) * randi_range(15, 30)
 	get_tree().root.add_child(asteroid)
+	wave_budget -= Asteroid.cost
--- asteroid-survivors-game-project/scripts/asteroid.gd
+++ asteroid-survivors-game-project/scripts/asteroid.gd
@@ -3,9 +3,35 @@ extends Node2D
 
 @onready var sprite: Sprite2D = $Sprite2D
 var velocity: Vector2 = Vector2.ZERO
+var size: int = 3
+var reinforced: bool = false
+var exploder: bool = false
+
+enum MODIFIERS {
+	REINFORCED,
+	EXPLODER
+}
+
+var modifier_values: Dictionary[MODIFIERS, bool] = {
+	MODIFIERS.REINFORCED : false,
+	MODIFIERS.EXPLODER : false
+}
+
+const cost: int = 5
 
 func _ready() -> void:
 	sprite.rotation = velocity.angle()
+	print("asteroid created")
 
 func _process(delta: float) -> void:
 	position += velocity * delta
+
+func apply_modifier():
+	var modifier_keys = modifier_values.keys()
+	var apply_index = modifier_keys.pick_random()
+	var init_apply_index = apply_index
+	while (modifier_values[apply_index]):
+		apply_index = (apply_index + 1) % modifier_keys.size()
+		if (apply_index == init_apply_index):
+			break
+	modifier_values[apply_index] = true
